5070f626459f5dc60eba55cca99117d6c2f16b61
V1ncen11 <kevin10222075@sttcipasung.ac.id>
2026-02-21 20:47:32 +0700

    Final: Fix manifest hidden folder and routing assets

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    // TAMBAHIN INI: Biar manifest langsung di public/build/manifest.json
+    build: {
+        manifest: 'manifest.json',
+    },
 });
