aa2a248fbbf0959208672c74717a0eccc3460aa3
V1ncen11 <kevin10222075@sttcipasung.ac.id>
2026-02-23 11:19:20 +0700

    edit buttn

--- resources/views/admin/produk/index.blade.php
+++ resources/views/admin/produk/index.blade.php
@@ -23,7 +23,7 @@
                 Tambah Jasa / Template
             </a>
 
-            <a href="{{ route('admin.produk.create', ['type' => 'portofolio']) }}" class="inline-flex items-center justify-center px-5 py-3 bg-purple-600 text-white font-bold rounded-2xl hover:bg-purple-700 hover:scale-105 transition-all shadow-lg shadow-purple-100">
+            <a href="{{ route('admin.produk.create', ['type' => 'portofolio']) }}" class="inline-flex items-center justify-center px-5 py-3 bg-purple-600 text-grey font-bold rounded-2xl hover:bg-purple-700 hover:scale-105 transition-all shadow-lg shadow-purple-100">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
