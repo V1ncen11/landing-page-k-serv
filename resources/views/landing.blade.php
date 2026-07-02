9c4bd2fb522b54a8c40f0b40311b8e3f92957b80
V1ncen11 <kevin10222075@sttcipasung.ac.id>
2026-07-03 00:36:18 +0700

    style: replace pengerjaan cepat image with character-based illustration v3

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -85,7 +85,7 @@
                 {{-- Cepat --}}
                 <div class="flex flex-col md:flex-row items-center gap-10 md:gap-20">
                   <div class="w-full md:w-1/2 flex justify-center mix-blend-multiply" style="mix-blend-mode: multiply;" data-aos="fade-right">
-                    <img src="/images/kserv_pengerjaan_v2.png" alt="Pengerjaan Cepat" class="w-[300px] md:w-[400px] h-auto object-contain" />
+                    <img src="/images/kserv_pengerjaan_v3.png" alt="Pengerjaan Cepat" class="w-[300px] md:w-[400px] h-auto object-contain" />
                   </div>
                   <div class="w-full md:w-1/2 text-center md:text-left" data-aos="fade-left">
                     <h3 class="text-3xl md:text-4xl font-bold mb-4 text-slate-900 tracking-tight">Pengerjaan Cepat</h3>
